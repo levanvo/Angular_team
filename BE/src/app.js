4edfb422ac602115d724f3ab96b01b00522f46e7
Lê Văn Võ <vole543215@gmail.com>
2023-06-12 16:44:59 +0700

    Update FE-BE

---
+++ BE/src/app.js
@@ -0,0 +1,18 @@
+import express from "express";
+import mongoose from "mongoose";
+import cors from "cors";
+import routerPr from "./routers/router_Pr";
+import routerAt from "./routers/router_At";
+import routerCt from "./routers/router_Ct";
+
+
+const app=express();
+app.use(express.json());
+app.use(cors());
+
+app.use(routerPr);
+app.use(routerAt);
+app.use(routerCt);
+
+mongoose.connect("mongodb://127.0.0.1:27017/Node");
+export const viteNodeApp=app;
