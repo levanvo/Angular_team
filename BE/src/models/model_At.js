bed83bf3610a1bf73a08cf2b9267d9215a5f23b5
Lê Văn Võ <vole543215@gmail.com>
2023-06-12 21:56:06 +0700

    Update FE+BE

---
+++ BE/src/models/model_At.js
@@ -0,0 +1,28 @@
+import mongoose, { mongo } from "mongoose"
+
+const SchemaMG_At = mongoose.Schema(
+    {
+        name: String,
+        image:String,
+        email: {
+            type:String,
+            unique:true,
+            required:true
+        },
+        pass: {
+            type:String,
+            required:true
+        },
+        repass:{
+            type:String,
+            required:true,
+        },
+        cart:[],
+        message:[],
+        status:Boolean,
+        role:String,
+    },
+    {timestamps:true,versionKey:false}
+);
+
+export default mongoose.model("authors",SchemaMG_At);
